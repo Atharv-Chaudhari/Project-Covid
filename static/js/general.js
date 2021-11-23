21b5d7c9c1ce84f6baece5f01349b700d4c2648c
Atharv Chaudhari <ahc382000@gmail.com>
2021-11-23 22:31:11 +0530

    added loading functionality

--- static/js/general.js
+++ static/js/general.js
@@ -4,5 +4,23 @@
 // }
 // document.cookie = "isDark=true";
 function therow() {
-    alertify.alert('<center><h1>Welcome</h1></center><br><center><h2 style="color:blue;">To The</h2><center><br><h2 style="color:red;">Project Covid</h2>');
+    alertify.alert('Team Infy SOARS Presents', '<center><h1>Welcome</h1></center><br><center><h2 style="color:blue;">To The</h2><center><br><h2 style="color:red;">Project Covid</h2>', function(){ alertify.success('Ok'); });
+}
+// function get_alert() {
+//     alertify.alert('Way To Risk Predictor', '<body style="z-index:99999;"><center><h1>Welcome</h1></center><br><center><h2 style="color:blue;">To The</h2><center><br><h2 style="color:red;">Project Covid</h2></body>', function(){ alertify.success('Ok'); });
+// }
+function get_alert(){
+    alert("Please Click On Icon  ↗  To Get UI in New Window");
+    }
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDivload").style.display = "block";
+  document.getElementById('mymarquee').start();
 }
