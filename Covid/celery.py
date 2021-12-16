cb17416a256466be39a4c005192c205ff747d380
Atharv-Chaudhari <ahc382000@gmail.com>
2021-12-16 08:26:38 +0530

    .

--- Covid/celery.py
+++ Covid/celery.py
@@ -16,11 +16,11 @@ import os
 broker_pool_limit = 1 # Will decrease connection usage
 broker_heartbeat = None # We're using TCP keep-alive instead
 broker_connection_timeout = 30 # May require a long timeout due to Linux DNS timeouts etc
-result_backend = None # AMQP is not recommended as result backend as it creates thousands of queues
+# result_backend = None # AMQP is not recommended as result backend as it creates thousands of queues
 event_queue_expires = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 worker_prefetch_multiplier = 1 # Disable prefetching, it's causes problems and doesn't help performance
 worker_concurrency = 50 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
-app = Celery('Covid', broker='amqps://xwwclfrs:n8Dz-5ooraTsgcmm_Vsu8WzDPYNPHRsx@jackal.rmq.cloudamqp.com/xwwclfrs')
+app = Celery('Covid', broker='amqps://xwwclfrs:n8Dz-5ooraTsgcmm_Vsu8WzDPYNPHRsx@jackal.rmq.cloudamqp.com/xwwclfrs', backend='amqps://xwwclfrs:n8Dz-5ooraTsgcmm_Vsu8WzDPYNPHRsx@jackal.rmq.cloudamqp.com/xwwclfrs')
 
 # app = Celery('Covid')
 
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn Covid.wsgi
+web: gunicorn Covid.wsgi --workers 1
+celery: celery -A Covid worker --without-heartbeat --without-gossip --without-mingle -l info -P eventlet
