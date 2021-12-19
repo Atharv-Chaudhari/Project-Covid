9bfb9a4d332c3cd630ca9263925e4396495dad64
Atharv-Chaudhari <ahc382000@gmail.com>
2021-12-19 12:03:27 +0530

    .

--- Covid/celery.py
+++ Covid/celery.py
@@ -19,7 +19,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Covid.settings')
 # event_queue_expires = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 # worker_prefetch_multiplier = 1 # Disable prefetching, it's causes problems and doesn't help performance
 # worker_concurrency = 50 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
-app = Celery('Covid', broker='amqps://xwwclfrs:n8Dz-5ooraTsgcmm_Vsu8WzDPYNPHRsx@jackal.rmq.cloudamqp.com/xwwclfrs',backend="http://0.0.0.0:14130")
+app = Celery('Covid', broker='amqps://xwwclfrs:n8Dz-5ooraTsgcmm_Vsu8WzDPYNPHRsx@jackal.rmq.cloudamqp.com/xwwclfrs')
 print(app.conf.broker_url)
 
 # app = Celery('Covid')
--- Covid/settings.py
+++ Covid/settings.py
@@ -14,8 +14,12 @@ from pathlib import Path
 import os
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
