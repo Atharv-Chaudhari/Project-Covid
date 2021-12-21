2db7f718328f290b962c80fa6ef5184ace255e30
Atharv-Chaudhari <ahc382000@gmail.com>
2021-12-21 21:38:00 +0530

    .

--- app/views.py
+++ app/views.py
@@ -34,13 +34,24 @@ def get_prediction (data,loaded_model = loaded_model):
     prediction=loaded_model.predict(df.values)
     pred_prob = loaded_model.predict_proba(df.values)
     print(prediction,pred_prob)
+    for x in data_model.keys():
+        if(x=='gender'):
+            if(data_model['gender']=='1'):
+                data_model[x]="Male"
+            else:
+                data_model[x]="Female"
+        else:
+            if(data_model[x]=='1'):
+                data_model[x]="Yes"
+            else:
+                data_model[x]="No"
     data_model['prob']=str(int(pred_prob[0][1]*100))+" %"
     data_model['prediction']=str(prediction[0])
     data_model['email']=data['email']
     data_model['country']=data['country']
     if(data_model['country']==''):
         data_model['country']="Not Given"
-    data_model['one']=['1']
+    data_model['one']=['Yes','1']
     send_mail_task.delay(data_model)
     return pred_prob
 
