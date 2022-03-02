e74a576bf1785f0c8f5007d85a01853e3502256f
Atharv-Chaudhari <ahc382000@gmail.com>
2022-03-02 16:25:16 +0530

    edits for urls.py removing pdf files

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
-from .views import GeneratePdf
 
 urlpatterns = [
     path("",views.welcome,name='app'),
