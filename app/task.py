9a936fb03a7f68b1db81b1869ff2ec0cd3b43517
Atharv-Chaudhari <ahc382000@gmail.com>
2022-03-03 14:18:57 +0530

    edits for gradcam

--- app/task.py
+++ app/task.py
@@ -62,29 +62,23 @@ def send_img_mail_task(data):
     cam_pred(data['img'],data['img'].replace("image","cam_pred"))
     token = os.environ['token']
     g = Github(token)
-    repo = g.get_user().get_repo('Project-Covid-Helper')
+    repo = g.get_user().get_repo('mathematics')
     file_list = [
         data['img'],
-        data['img'].replace("image","cam_pred"),
+        data['img'].replace("image","cam_pred")
     ]
-    file_names = [
-        'image1.png',
-        'cam_img.png',
-    ]
-    commit_message = 'Commit Images'
-    master_ref = repo.get_git_ref('heads/main')
+    commit_message = 'Add Images'
+    master_ref = repo.get_git_ref('heads/master')
     master_sha = master_ref.object.sha
     base_tree = repo.get_git_tree(master_sha)
-
     element_list = list()
-    for i, entry in enumerate(file_list):
-        with open(entry) as input_file:
+    for entry in file_list:
+        with open(entry, 'rb') as input_file:
             data = input_file.read()
-        if entry.endswith('.png'): 
+        if entry.endswith('.png'):
             data = base64.b64encode(data)
-        element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
+        element = InputGitTreeElement(entry, '100644', 'blob', data)
         element_list.append(element)
-
     tree = repo.create_git_tree(element_list, base_tree)
     parent = repo.get_git_commit(master_sha)
     commit = repo.create_git_commit(commit_message, tree, [parent])
