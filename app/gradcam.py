08adbd333b074546c418d8855674c6e72ffac88f
Atharv-Chaudhari <ahc382000@gmail.com>
2022-03-03 13:48:41 +0530

    commit gradcam edits

---
+++ app/gradcam.py
@@ -0,0 +1,61 @@
+def cam_pred(img_path,out_path):
+    import numpy as np
+    import tensorflow as tf
+    from tensorflow import keras
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    model_builder = keras.applications.xception.Xception
+    img_size = (299, 299)
+    preprocess_input = keras.applications.xception.preprocess_input
+    decode_predictions = keras.applications.xception.decode_predictions
+    last_conv_layer_name = "block14_sepconv2_act"
+
+    ## The GradCam Algorithm
+    def get_img_array(img_path, size):
+        img = keras.preprocessing.image.load_img(img_path, target_size=size)
+        array = keras.preprocessing.image.img_to_array(img)
+        array = np.expand_dims(array, axis=0)
+        return array
+
+
+    def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+        grad_model = tf.keras.models.Model(
+            [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+        )
+        with tf.GradientTape() as tape:
+            last_conv_layer_output, preds = grad_model(img_array)
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0])
+            class_channel = preds[:, pred_index]
+        grads = tape.gradient(class_channel, last_conv_layer_output)
+        pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+        last_conv_layer_output = last_conv_layer_output[0]
+        heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+        heatmap = tf.squeeze(heatmap)
+        heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+        return heatmap.numpy()
+
+    ## Heatmap:
+    img_array = preprocess_input(get_img_array(img_path, size=img_size))
+    model = model_builder(weights="imagenet")
+    model.layers[-1].activation = None
+    preds = model.predict(img_array)
+    heatmap = make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+
+    ## Superimposed Viz:
+    def save_and_display_gradcam(img_path, heatmap, cam_path=out_path, alpha=0.4):
+        img = keras.preprocessing.image.load_img(img_path)
+        img = keras.preprocessing.image.img_to_array(img)
+        heatmap = np.uint8(255 * heatmap)
+        jet = cm.get_cmap("jet")
+        jet_colors = jet(np.arange(256))[:, :3]
+        jet_heatmap = jet_colors[heatmap]
+        jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+        jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+        jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+        superimposed_img = jet_heatmap * alpha + img
+        superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+        superimposed_img.save(cam_path)
+        print(superimposed_img)
+    
+    save_and_display_gradcam(img_path,heatmap)
--- app/task.py
+++ app/task.py
@@ -8,16 +8,16 @@ import json
 from django.template.loader import get_template
 from django.http import HttpResponse
 from django.views.generic import View
- 
+from .gradcam import *
 #importing get_template from loader
 from django.template.loader import get_template
- 
-# from fpdf import FPDF, HTMLMixin
-
-
-# class PDF(FPDF, HTMLMixin):
-#     pass
-
+from PIL import Image
+import base64
+import io
+import base64
+from github import Github
+from github import InputGitTreeElement
+import os
 
 @shared_task
 def sleepy(duration):
@@ -44,7 +44,6 @@ def world_data(d):
     r = requests.get(URL)
 
     soup = BeautifulSoup(r.content, 'html5lib')
-    # print(soup.select(".maincounter-number span"))
     nums = soup.find_all('div', attrs={'class': 'maincounter-number'})
     wdata = []
     for tag in nums:
@@ -60,13 +59,42 @@ def world_data(d):
 
 @shared_task
 def send_img_mail_task(data):
-    # grad cam code here take static path of cam img
-    cam_img="static/tmp/pred_img.jpg" # path to img
+    cam_pred(data['img'],data['img'].replace("image","cam_pred"))
+    token = os.environ['token']
+    g = Github(token)
+    repo = g.get_user().get_repo('Project-Covid-Helper')
+    file_list = [
+        data['img'],
+        data['img'].replace("image","cam_pred"),
+    ]
+    file_names = [
+        data['img'],
+        data['img'].replace("image","cam_pred"),
+    ]
+    commit_message = 'Commit Images'
+    master_ref = repo.get_git_ref('heads/main')
+    master_sha = master_ref.object.sha
+    base_tree = repo.get_git_tree(master_sha)
+
+    element_list = list()
+    for i, entry in enumerate(file_list):
+        with open(entry) as input_file:
+            data = input_file.read()
+        if entry.endswith('.png'): 
+            data = base64.b64encode(data)
+        element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
+        element_list.append(element)
+
+    tree = repo.create_git_tree(element_list, base_tree)
+    parent = repo.get_git_commit(master_sha)
+    commit = repo.create_git_commit(commit_message, tree, [parent])
+    master_ref.edit(commit.sha)
     ctx = {
-        'output': data['output'],
+        'tk':[1],
+        'output': int(data['output']),
         'email':data['email'],
-        'img':"https://raw.githubusercontent.com/Atharv-Chaudhari/Project-Covid/main/"+str(data['img']),
-        'cam_img':"https://raw.githubusercontent.com/Atharv-Chaudhari/Project-Covid/main/"+str(cam_img),
+        'img':'',
+        'cam_img':'',
     }
     heading = "Test Report By Team InfySOARS"
     messageContent = get_template('img_email.html').render(ctx)
