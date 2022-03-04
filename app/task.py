5dd7ce1a3f60c8f251023fab9bf4b0b3d5d69d9d
Atharv-Chaudhari <ahc382000@gmail.com>
2022-03-04 13:23:56 +0530

    Email Page For Img Predictor Completed

--- app/task.py
+++ app/task.py
@@ -9,7 +9,7 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 from django.views.generic import View
 from .gradcam import *
-#importing get_template from loader
+# importing get_template from loader
 from django.template.loader import get_template
 from PIL import Image
 import base64
@@ -18,6 +18,7 @@ import base64
 from github import Github
 from github import InputGitTreeElement
 import os
+
 
 @shared_task
 def sleepy(duration):
@@ -38,6 +39,7 @@ def send_mail_task(data):
     msg.send()
     return None
 
+
 @shared_task
 def world_data(d):
     URL = "https://www.worldometers.info/coronavirus/"
@@ -57,70 +59,68 @@ def world_data(d):
     f = json.dump(d, f)
     return None
 
+
 @shared_task
 def send_img_mail_task(data):
-    cam_pred(data['img'],data['img'].replace("image","cam_pred"))
-    
-    token = os.environ['token']
-    g = Github(token)
-    repo = g.get_user().get_repo('Project-Covid-Helper') # repo name
-    print(repo)
-    file_list = [
-        data['img'],
-        data['img'].replace("image","cam_pred")
-    ]
-    file_names = [
-        str(list(data['img'].split('/'))[-1]),
-        str(list(data['img'].replace("image","cam_pred").split('/'))[-1])
-    ]
-    commit_message = 'python commit'
-    master_ref = repo.get_git_ref('heads/main')
-    master_sha = master_ref.object.sha
-    base_tree = repo.get_git_tree(master_sha)
-
-    element_list = list()
-    for i, entry in enumerate(file_list):
-        with open(entry,'rb') as input_file:
-            data1 = input_file.read()
-        if entry.endswith('.png'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.jpeg'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.jfif'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.pjpeg'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.bmp'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.webp'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.pjp'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.tif'): 
-            data1 = base64.b64encode(data1)
-        if entry.endswith('.jpg'): 
-            data1 = base64.b64encode(data1)
-        blob = repo.create_git_blob(data1.decode("utf-8"), "base64")
-        element = InputGitTreeElement(file_names[i], mode='100644', type='blob',sha=blob.sha)
-        element_list.append(element)
-
-    tree = repo.create_git_tree(element_list, base_tree)
-    parent = repo.get_git_commit(master_sha)
-    commit = repo.create_git_commit(commit_message, tree, [parent])
-    master_ref.edit(commit.sha)
-    import time
-    time.sleep(10)
+    cam_pred(data['img'], data['img'].replace("image", "cam_pred"))
     ctx = {
-        'tk':[1],
+        'tk': [1],
         'output': int(data['output']),
-        'email':data['email'],
-        'img':str("https://raw.githubusercontent.com/INFYSOARS/Project-Covid-Helper/main/")+file_names[0],
-        'cam_img':str("https://raw.githubusercontent.com/INFYSOARS/Project-Covid-Helper/main/")+file_names[1],
+        'email': data['email'],
+        'img':str(data['img'].split("/")[-1]),
+        'cam_img':str(data['img'].replace("image", "cam_pred").split("/")[-1]),
     }
-    heading = "Test Report By Team InfySOARS"
+    # heading = "Test Report By Team InfySOARS"
     messageContent = get_template('img_email.html').render(ctx)
-    msg = EmailMessage(heading, messageContent, '<infysoars0@gmail.com>',
-                       [data['email']])
-    msg.content_subtype = 'html'
-    msg.send()
+    # msg = EmailMessage(heading, messageContent, '<infysoars0@gmail.com>',
+    #                    [data['email']])
+    # msg.content_subtype = 'html'
+    # msg.send()
+
+    import smtplib
+    import ssl
+    import email
+    from email import encoders
+    from email.mime.base import MIMEBase
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    sender_email = "infysoars0@gmail.com"
+    receiver_email = data['email']
+    password = os.environ.get("email_pass")
+
+    # Create MIMEMultipart object
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = "Test Report By Team InfySOARS"
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    filename = [data['img'],
+                data['img'].replace("image", "cam_pred")]
+
+    part = MIMEText(messageContent, "html")
+    msg.attach(part)
+
+    # Add Attachment
+    for i in range(len(filename)):
+        with open(filename[i], "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+            encoders.encode_base64(part)
+
+            # Set mail headers
+            part.add_header(
+                "Content-Disposition",
+                "attachment", filename=filename[i].split("/")[-1]
+            )
+            part.add_header('Content-ID', '<'+filename[i].split("/")[-1]+'>')
+            msg.attach(part)
+
+    # Create secure SMTP connection and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, msg.as_string()
+        )
     return None
